bf1766af1c45eeadbe4d8cece57b1e86a77301d3
nafezly <nafezly.com@gmail.com>
2022-10-02 23:32:29 +0200

    fix paypal

--- src/Classes/PayPalPayment.php
+++ src/Classes/PayPalPayment.php
@@ -106,7 +106,7 @@ class PayPalPayment implements PaymentInterface
         $client = new PayPalHttpClient($environment);
 
         try {
-            $response = $client->execute(new OrdersGetRequest($request['token']));
+            $response = $client->execute(new OrdersCaptureRequest($request['token']) );
             $result = json_decode(json_encode($response), true);
             if ($result['result']['intent'] == "CAPTURE" && $result['result']['status'] == "APPROVED") {
                 return [
