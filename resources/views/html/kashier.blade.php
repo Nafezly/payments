bad940e608078f3fb7554edd965b6695f6079906
unknown <ahmedsaoud31@gmail.com>
2022-10-06 12:46:09 +0200

    Use blade as html verification template for Kashier Payment Gateway

---
+++ resources/views/html/kashier.blade.php
@@ -0,0 +1,16 @@
+<body>
+<script id="kashier-iFrame"
+src="{{ $model->kashier_url }}/kashier-checkout.js"
+data-amount="{{ $model->amount }}"
+data-description="Credit"
+data-mode="{{ $model->kashier_mode }}"
+data-hash="{{ $data['hash'] }}"
+data-currency="{{ $data['currency'] "
+data-orderId="{{ $data['order_id'] "
+data-allowedMethods="card"
+data-merchantId="{{ $data['mid'] }}"
+data-merchantRedirect="{{ $data['redirect_back'] }}" 
+data-store="{{ $model->app_name }}"
+data-type="external" data-display="ar">
+</script>
+</body>
