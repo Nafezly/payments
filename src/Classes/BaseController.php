bc7546740ba5be058d590d3ff571de60c232e7d8
nafezly <nafezly.com@gmail.com>
2022-10-08 14:16:00 +0200

    updates

--- src/Classes/BaseController.php
+++ src/Classes/BaseController.php
@@ -4,7 +4,6 @@ namespace Nafezly\Payments\Classes;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
 use Nafezly\Payments\Traits\SetVariables;
 use Nafezly\Payments\Traits\SetRequiredFields;
 
