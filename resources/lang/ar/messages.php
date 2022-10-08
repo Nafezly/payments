de6e9046ba872d6039e92fbbde139e5801508f40
nafezly <nafezly.com@gmail.com>
2022-10-08 22:39:04 +0200

    updates

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -7,9 +7,11 @@ return [
     'Process_Has_Been_Blocked_From_System'=>"تم رفض المعاملة من قبل النظام",
     'Security_checks_are_not_passed_by_the_system'=>"لم يتم اجتياز اختبارات الحماية من قبل النظام",
     "Balance_is_not_enough"=>"الرصيد غير كافي",
+    'Declined'=>"العملية مرفوضة",
     'Your_card_is_not_secured_with_3D_protection_Check_with_the_bank'=>"البطاقة الخاصة بك غير مؤمنة بحماية 3D قم بمراجعة البنك",
     'Incorrect_card_expiration_date'=>"تاريخ انتهاء صلاحية البطاقة غير صحيح",
     "The_transaction_was_rejected_by_your_bank_please_check_with_your_bank"=>"تم رفض المعاملة من قبل البنك الخاص بك، برجاء مراجعة البنك الخاص بك",
     "The_OTP_number_was_entered_incorrectly"=>"تم إدخال رقم OTP بشكل غير صحيح",
-    "An_error_occurred_while_executing_the_operation"=>"حدث خطأ أثناء تنفيذ العملية"
+    "An_error_occurred_while_executing_the_operation"=>"حدث خطأ أثناء تنفيذ العملية",
+    'Your_card_is_not_authorized_with_3D_secure'=>"لم يتم تأكيد رمز OTP بنجاح",
 ];
--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -7,9 +7,11 @@ return [
     'Process_Has_Been_Blocked_From_System'=>"Process Has Been Blocked From System",
     'Security_checks_are_not_passed_by_the_system'=>"Security checks are not passed by the system",
     "Balance_is_not_enough"=>"Balance is not enough",
+    'Declined'=>'Declined',
     'Your_card_is_not_secured_with_3D_protection_Check_with_the_bank'=>"Your card is not secured with 3D protection",
     'Incorrect_card_expiration_date'=>"Incorrect card expiration date",
     "The_transaction_was_rejected_by_your_bank_please_check_with_your_bank"=>"The transaction was rejected by your bank please check with your bank",
     "The_OTP_number_was_entered_incorrectly"=>"The OTP number was entered incorrectly",
-    "An_error_occurred_while_executing_the_operation"=>"An error occurred while executing the operation"
+    "An_error_occurred_while_executing_the_operation"=>"An error occurred while executing the operation",
+    'Your_card_is_not_authorized_with_3D_secure'=>"Your card is not authorized with 3D secure"
 ];
