952ba1a8290f889e52d2d8d7efe66d08170adc8b
nafezly <nafezly.com@gmail.com>
2023-10-09 05:10:42 +0300

    m

--- resources/views/html/stripe.blade.php
+++ resources/views/html/stripe.blade.php
@@ -1,6 +1,6 @@
 <script src="https://js.stripe.com/v3/"></script>
 <style>
-  * {
+    * {
   box-sizing: border-box;
 }
 .hidden {
@@ -114,125 +114,130 @@
 }
 </style>
 <div class="stripe-container">
-  <form id="payment-form">
-  <div id="payment-element">
-  </div>
-  <button id="submit-button-stripe">
-  <div class="spinner hidden" id="spinner"></div>
-  <span id="button-text">ادفع بأمان</span>
-  </button>
-  <div id="payment-message" class="hidden"></div>
-  </form>
+    <form id="payment-form">
+        <div id="payment-element">
+        </div>
+        <div id="btn-stripe-container"></div>
+        <div id="payment-message" class="hidden"></div>
+    </form>
 </div>
-
 <script type="text/javascript">
-
 document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+
+
+        let btn_ctr = document.getElementById("btn-stripe-container");
+        btn_ctr.append('<button id="submit-button-stripe"><div class="spinner hidden" id="spinner"></div><span id="button-text">ادفع بأمان</span></button>');
 
 
 
-const stripe = Stripe("{{$data['public_key']}}"); 
-let elements;
-const appearance = {
-  clientSecret:'{{$data['client_secret']}}',
-  locale:'ar',
-  theme: 'night',
-  variables: { colorPrimaryText: '#2196f3' }
 
-};
-const options = {
-  layout: {
-    type: 'tabs',
-    defaultCollapsed: false,
-  },
-  fields: {
-        billingDetails: {
-            address: {
-                country: 'never',
-                postalCode: 'never'
-            }
-        }
-    }
-};
-initialize();
-checkStatus();
-document.querySelector("#payment-form").addEventListener("submit", handleSubmit);
-async function initialize() {
-    elements = stripe.elements(appearance);
-    const paymentElement = elements.create("payment",options);
-    paymentElement.mount("#payment-element");
-}
-async function handleSubmit(e) {
-    e.preventDefault();
-    setLoading(true);
 
-    const { error } = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-            return_url: "{{$data['return_url']}}",
-            payment_method_data:{
-                billing_details:{
-                    address:{
-                        country:'SA',
-                        postal_code:'00000'
+        const stripe = Stripe("{{$data['public_key']}}");
+        let elements;
+        const appearance = {
+            clientSecret: '{{$data['
+            client_secret ']}}',
+            locale: 'ar',
+            theme: 'night',
+            variables: { colorPrimaryText: '#2196f3' }
+
+        };
+        const options = {
+            layout: {
+                type: 'tabs',
+                defaultCollapsed: false,
+            },
+            fields: {
+                billingDetails: {
+                    address: {
+                        country: 'never',
+                        postalCode: 'never'
                     }
                 }
             }
-        },
-    });
-    if (error.type === "card_error" || error.type === "validation_error") {
-        showMessage(error.message);
-    } else {
-        showMessage("خطأ غير متوقع.");
-    }
-    setLoading(false);
-}
+        };
+        initialize();
+        checkStatus();
+        document.querySelector("#payment-form").addEventListener("submit", handleSubmit);
+        async function initialize() {
+            elements = stripe.elements(appearance);
+            const paymentElement = elements.create("payment", options);
+            paymentElement.mount("#payment-element");
+        }
+        async function handleSubmit(e) {
+            e.preventDefault();
+            setLoading(true);
 
-async function checkStatus() {
-    const clientSecret = new URLSearchParams(window.location.search).get(
-        "payment_intent_client_secret"
-    );
-    if (!clientSecret) {
-        return;
-    }
-    const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
-    switch (paymentIntent.status) {
-        case "succeeded":
-            showMessage("تم الدفع بنجاح!");
-            break;
-        case "processing":
-            showMessage("جار تنفيذ العملية.");
-            break;
-        case "requires_payment_method":
-            showMessage("لم تتم عملية الدفع بنجاح، برجاء المحاولة مرة أخرى.");
-            break;
-        default:
-            showMessage("حدث خطأ ما.");
-            break;
-    }
-}
+            const { error } = await stripe.confirmPayment({
+                elements,
+                confirmParams: {
+                    return_url: "{{$data['return_url']}}",
+                    payment_method_data: {
+                        billing_details: {
+                            address: {
+                                country: 'SA',
+                                postal_code: '00000'
+                            }
+                        }
+                    }
+                },
+            });
+            if (error.type === "card_error" || error.type === "validation_error") {
+                showMessage(error.message);
+            } else {
+                showMessage("خطأ غير متوقع.");
+            }
+            setLoading(false);
+        }
 
-function showMessage(messageText) {
-    const messageContainer = document.querySelector("#payment-message");
-    messageContainer.classList.remove("hidden");
-    messageContainer.textContent = messageText;
+        async function checkStatus() {
+            const clientSecret = new URLSearchParams(window.location.search).get(
+                "payment_intent_client_secret"
+            );
+            if (!clientSecret) {
+                return;
+            }
+            const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+            switch (paymentIntent.status) {
+                case "succeeded":
+                    showMessage("تم الدفع بنجاح!");
+                    break;
+                case "processing":
+                    showMessage("جار تنفيذ العملية.");
+                    break;
+                case "requires_payment_method":
+                    showMessage("لم تتم عملية الدفع بنجاح، برجاء المحاولة مرة أخرى.");
+                    break;
+                default:
+                    showMessage("حدث خطأ ما.");
+                    break;
+            }
+        }
 
-    setTimeout(function () {
-        messageContainer.classList.add("hidden");
-        messageText.textContent = "";
-    }, 4000);
-}
-function setLoading(isLoading) {
-    if (isLoading) {
-        document.querySelector("#submit-button-stripe").disabled = true;
-        document.querySelector("#spinner").classList.remove("hidden");
-        document.querySelector("#button-text").classList.add("hidden");
-    } else {
-        document.querySelector("#submit-button-stripe").disabled = false;
-        document.querySelector("#spinner").classList.add("hidden");
-        document.querySelector("#button-text").classList.remove("hidden");
-    }
-}
+        function showMessage(messageText) {
+            const messageContainer = document.querySelector("#payment-message");
+            messageContainer.classList.remove("hidden");
+            messageContainer.textContent = messageText;
 
+            setTimeout(function() {
+                messageContainer.classList.add("hidden");
+                messageText.textContent = "";
+            }, 4000);
+        }
+
+        function setLoading(isLoading) {
+            if (isLoading) {
+                document.querySelector("#submit-button-stripe").disabled = true;
+                document.querySelector("#spinner").classList.remove("hidden");
+                document.querySelector("#button-text").classList.add("hidden");
+            } else {
+                document.querySelector("#submit-button-stripe").disabled = false;
+                document.querySelector("#spinner").classList.add("hidden");
+                document.querySelector("#button-text").classList.remove("hidden");
+            }
+        }
+    }, 2000);
 });
+
 </script>
