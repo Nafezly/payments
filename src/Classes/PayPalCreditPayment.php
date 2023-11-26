aab0fa1647b24f7c135b7c482c18e35a5068198e
nafezly <nafezly.com@gmail.com>
2023-11-26 23:15:53 +0200

    m

--- src/Classes/PayPalCreditPayment.php
+++ src/Classes/PayPalCreditPayment.php
@@ -71,7 +71,7 @@ class PayPalCreditPayment extends BaseController implements PaymentInterface
                     "experience_context" => [
                         "payment_method_preference" => "UNRESTRICTED",
                         "locale" => "ar-SA",
-                        "shipping_preference" => "NO_SHIPPING",
+                        "shipping_preference" => "GET_FROM_FILE",
                         "return_url" => route($this->verify_route_name,['payment'=>'paypal_credit']),
                         "cancel_url" => route($this->verify_route_name,['payment'=>'paypal_credit']),
                     ],
