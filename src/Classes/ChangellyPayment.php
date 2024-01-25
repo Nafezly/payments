2293c02533b76c5b9377b71c648a8bfdc0bfae4e
nafezly <nafezly.com@gmail.com>
2024-01-25 06:51:06 +0200

    m

---
+++ src/Classes/ChangellyPayment.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Nafezly\Payments\Classes;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Nafezly\Payments\Exceptions\MissingPaymentInfoException;
+use Nafezly\Payments\Interfaces\PaymentInterface;
+use Nafezly\Payments\Classes\BaseController;
+
+class ChangellyPayment extends BaseController implements PaymentInterface
+{
+    private $changelly_api_key;
+    private $changelly_private_key;
+    private $paymob_wallet_integration_id;
+
+    public function __construct()
+    {
+        $this->changelly_api_key = config('nafezly-payments.CHANGELLY_API_KEY');
+        $this->changelly_private_key = config('nafezly-payments.CHANGELLY_PRIVATE_KEY');
+        $this->currency = config("nafezly-payments.PAYMOB_CURRENCY");
+        $this->paymob_wallet_integration_id = config("nafezly-payments.PAYMOB_WALLET_INTEGRATION_ID");
+    }
+
+    /**
+     * @param $amount
+     * @param null $user_id
+     * @param null $user_first_name
+     * @param null $user_last_name
+     * @param null $user_email
+     * @param null $user_phone
+     * @param null $source
+     * @return void
+     * @throws MissingPaymentInfoException
+     */
+    public function pay($amount = null, $user_id = null, $user_first_name = null, $user_last_name = null, $user_email = null, $user_phone = null, $source = null)
+    {
+        $this->setPassedVariablesToGlobal($amount,$user_id,$user_first_name,$user_last_name,$user_email,$user_phone,$source);
+        $required_fields = ['amount','user_email'];
+        $this->checkRequiredFields($required_fields, 'Changelly');
+
+
+        $timestamp = date('Y-m-d H:i:s');
+        $body = [
+            
+                "nominal_currency" => "string",
+                "nominal_amount" => "14.08",
+                "title" => "DBA",
+                "description" => "string",
+                "order_id" => "121215151",
+                "customer_id" => "string",
+                "customer_email" => "johndoe@example.com",
+                "success_redirect_url" => "https://example.com/payment/success",
+                "failure_redirect_url" => "https://example.com/payment/failure",
+                "fees_payer" => "MERCHANT",
+                "payment_data" => [
+                    "sales_manager_id" => 221069,
+                    "customer_phone" => "+999999999",
+                ],
+                "pending_deadline_at" => "2023-03-22T15:23:56.876Z",
+            
+        ];
+        dd(str_replace('\/','/',json_encode($body)));
+        
+        $payload= "POST:/api/payment/v1/payments:". str_replace('\/','/',json_encode($body)) .":".$timestamp;
+        $key= base64_encode(base64_encode(hash_hmac('sha256', hash('sha256', $payload) , $this->changelly_private_key)) . ":".$timestamp);
+
+
+        //dd($key);
+
+        $response = \Http::withHeaders([
+            "X-Api-Key" => $this->changelly_api_key,
+            'X-Signature'=> $key,
+            'Content-type'=>'application/json'
+        ])
+       ->post('https://api.pay.changelly.com/api/payment/v1/payments',$body);
+        dd($response->status());
+
+
+        $get_order = Http::withHeaders(['content-type' => 'application/json'])
+            ->post('https://accept.paymobsolutions.com/api/ecommerce/orders', [
+                "auth_token" => $request_new_token['token'],
+                "delivery_needed" => "false",
+                "amount_cents" => $this->amount * 100,
+                "items" => []
+            ])->json();
+        $get_url_token = Http::withHeaders(['content-type' => 'application/json'])
+            ->post('https://accept.paymobsolutions.com/api/acceptance/payment_keys', [
+                "auth_token" => $request_new_token['token'],
+                "expiration" => 36000,
+                "amount_cents" => $get_order['amount_cents'],
+                "order_id" => $get_order['id'],
+                "billing_data" => [
+                    "apartment" => "NA",
+                    "email" => $this->user_email,
+                    "floor" => "NA",
+                    "first_name" => $this->user_first_name,
+                    "street" => "NA",
+                    "building" => "NA",
+                    "phone_number" => $this->user_phone,
+                    "shipping_method" => "NA",
+                    "postal_code" => "NA",
+                    "city" => "NA",
+                    "country" => "NA",
+                    "last_name" => $this->user_last_name,
+                    "state" => "NA"
+                ],
+                "currency" => $this->currency,
+                "integration_id" => $this->paymob_wallet_integration_id,
+                'lock_order_when_paid'=>true
+            ])->json();
+
+        $get_pay_link = Http::withHeaders(['content-type' => 'application/json'])
+            ->post('https://accept.paymob.com/api/acceptance/payments/pay', [
+                'source'=>[
+                    "identifier"=>$this->user_phone,
+                    'subtype'=>"WALLET"
+                ],
+                "payment_token"=>$get_url_token['token']
+        ])->json(); 
+        return [
+            'payment_id'=>$get_order['id'],
+            'html' => "",
+            'redirect_url'=>$get_pay_link['redirect_url']
+        ];
+        
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function verify(Request $request): array
+    {
+        $string = $request['amount_cents'] . $request['created_at'] . $request['currency'] . $request['error_occured'] . $request['has_parent_transaction'] . $request['id'] . $request['integration_id'] . $request['is_3d_secure'] . $request['is_auth'] . $request['is_capture'] . $request['is_refunded'] . $request['is_standalone_payment'] . $request['is_voided'] . $request['order'] . $request['owner'] . $request['pending'] . $request['source_data_pan'] . $request['source_data_sub_type'] . $request['source_data_type'] . $request['success'];
+
+        if (hash_hmac('sha512', $string, config('nafezly-payments.PAYMOB_HMAC'))) {
+            if ($request['success'] == "true") {
+                return [
+                    'success' => true,
+                    'payment_id'=>$request['order'],
+                    'message' => __('nafezly::messages.PAYMENT_DONE'),
+                    'process_data' => $request->all()
+                ];
+            } else {
+                return [
+                    'success' => false,
+                    'payment_id'=>$request['order'],
+                    'message' => __('nafezly::messages.PAYMENT_FAILED_WITH_CODE',['CODE'=>$this->getErrorMessage($request['txn_response_code'])]),
+                    'process_data' => $request->all()
+                ];
+            }
+
+        } else {
+            return [
+                'success' => false,
+                'payment_id'=>$request['order'],
+                'message' => __('nafezly::messages.PAYMENT_FAILED'),
+                'process_data' => $request->all()
+            ];
+        }
+    }
+    public function getErrorMessage($code){
+        $errors=[
+            'BLOCKED'=>__('nafezly::messages.Process_Has_Been_Blocked_From_System'),
+            'B'=>__('nafezly::messages.Process_Has_Been_Blocked_From_System'),
+            '5'=>__('nafezly::messages.Balance_is_not_enough'),
+            'F'=>__('nafezly::messages.Your_card_is_not_authorized_with_3D_secure'),
+            '7'=>__('nafezly::messages.Incorrect_card_expiration_date'),
+            '2'=>__('nafezly::messages.Declined'),
+            '6051'=>__('nafezly::messages.Balance_is_not_enough'),
+            '637'=>__('nafezly::messages.The_OTP_number_was_entered_incorrectly'),
+            '11'=>__('nafezly::messages.Security_checks_are_not_passed_by_the_system'),
+        ];
+        if(isset($errors[$code]))
+            return $errors[$code];
+        else
+            return __('nafezly::messages.An_error_occurred_while_executing_the_operation');
+    }
+}
---
+++ src/Classes/WisePayment.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace Nafezly\Payments\Classes;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Nafezly\Payments\Exceptions\MissingPaymentInfoException;
+use Nafezly\Payments\Interfaces\PaymentInterface;
+use Nafezly\Payments\Classes\BaseController;
+
+class WisePayment extends BaseController implements PaymentInterface
+{
+    private $wise_api_key;
+    private $wise_balance_id;
+    private $wise_profile_id;
+    private $verify_route_name;
+
+    public function __construct()
+    {
+        $this->wise_api_key = config('nafezly-payments.WISE_API_KEY');
+        $this->wise_balance_id = config('nafezly-payments.WISE_BALANCE_ID');
+        $this->wise_profile_id = config('nafezly-payments.WISE_PROFILE_ID');
+        $this->verify_route_name = config('nafezly-payments.VERIFY_ROUTE_NAME');
+    }
+
+    /**
+     * @param $amount
+     * @param null $user_id
+     * @param null $user_first_name
+     * @param null $user_last_name
+     * @param null $user_email
+     * @param null $user_phone
+     * @param null $source
+     * @return Application|RedirectResponse|Redirector
+     * @throws MissingPaymentInfoException
+     */
+    public function pay($amount = null, $user_id = null, $user_first_name = null, $user_last_name = null, $user_email = null, $user_phone = null, $source = null)
+    {
+        $this->setPassedVariablesToGlobal($amount,$user_id,$user_first_name,$user_last_name,$user_email,$user_phone,$source);
+        $required_fields = ['amount'];
+        $this->checkRequiredFields($required_fields, 'Wise');
+      
+        try{
+            $payer_name = null;
+            if($this->user_first_name." ".$this->user_last_name!=" ")
+                $payer_name=$this->user_first_name." ".$this->user_last_name;
+
+            $selectedPaymentMethods_init=["WISE_ACCOUNT","ACCOUNT_DETAILS"/*,"CARD"*/];
+            if(is_array($this->source))
+                $selectedPaymentMethods_init=$this->source;
+        
+            $init = \Http::withHeaders([
+                'Cookie'=>"oauthToken=".$this->wise_api_key,
+                'Authorization'=>"Bearer ".$this->wise_api_key
+            ])->get('https://wise.com/gateway/v3/profiles/'.$this->wise_profile_id.'/acquiring/payment-methods', [
+                'currency' => 'USD',
+                'amount' => $this->amount,
+            ]);
+
+            $available_payment_methods = [];
+
+            foreach($selectedPaymentMethods_init as $check_available_method)
+                if(in_array($check_available_method,collect($init->json()['content'])->where('available',true)->pluck('paymentMethodType')->toArray()))
+                    $available_payment_methods[]=$check_available_method;
+
+            $create_payment = \Http::withHeaders([
+                'Host' => 'wise.com',
+                'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0',
+                'Accept' => 'application/json, text/plain, */*',
+                'Accept-Language' => 'en-GB',
+                'Accept-Encoding' => 'gzip, deflate, br',
+                'Content-Type' => 'application/json',
+                'X-Access-Token' => 'Tr4n5f3rw153',
+                'Content-Length' => 150,
+                'Origin' => 'https://wise.com',
+                'Referer' => 'https://wise.com/flows/request/',
+                'Sec-Fetch-Dest' => 'empty',
+                'Sec-Fetch-Mode' => 'cors',
+                'Sec-Fetch-Site' => 'same-origin',
+                'Te' => 'trailers',
+                'Cookie'=>"oauthToken=".$this->wise_api_key,
+                'Authorization'=>"Bearer ".$this->wise_api_key
+            ])->post('https://wise.com/gateway/v2/profiles/'.$this->wise_profile_id.'/acquiring/payment-requests', [
+                'requestType' => 'SINGLE_USE',
+                'amountValue' => $this->amount,
+                'balanceId' => $this->wise_balance_id,
+                'payer' => [
+                    'name' => $payer_name,
+                ],
+                'selectedPaymentMethods' => $available_payment_methods,
+            ]);
+            $create_payment_response = $create_payment->json();
+            $publish_payment_response = \Http::withHeaders([
+                'Host' => 'wise.com',
+                'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0',
+                'Accept' => 'application/json, text/plain, */*',
+                'Accept-Language' => 'en-GB',
+                'Accept-Encoding' => 'gzip, deflate, br',
+                'Content-Type' => 'application/json',
+                'X-Access-Token' => 'Tr4n5f3rw153',
+                'X-Language' => 'en',
+                'Content-Length' => 22,
+                'Origin' => 'https://wise.com',
+                'Referer' => 'https://wise.com/flows/request/',
+                'Sec-Fetch-Dest' => 'empty',
+                'Sec-Fetch-Mode' => 'cors',
+                'Sec-Fetch-Site' => 'same-origin',
+                'Te' => 'trailers',
+                'Cookie'=>"oauthToken=".$this->wise_api_key,
+                'Authorization'=>"Bearer ".$this->wise_api_key
+            ])->put('https://wise.com/gateway/v2/profiles/'.$this->wise_profile_id.'/acquiring/payment-requests/'.$create_payment_response['id'].'/status', [
+                'status' => 'PUBLISHED',
+            ]);
+            $responseData = $publish_payment_response->json();
+            return [
+                'payment_id'=>$responseData['id'],
+                'html' => $responseData,
+                'redirect_url'=>$responseData['link']
+            ];
+        }catch(\Exception $e){
+            return [
+                'payment_id'=>"",
+                'html' => $e,
+                'redirect_url'=>""
+            ];
+        }
+
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function verify(Request $request): array
+    {
+        $paid = 0;
+        try{
+            $response = Http::withHeaders([
+                'time-zone' => 'Africa/Cairo',
+                'Cookie'=>"oauthToken=".$this->wise_api_key,
+                'Authorization'=>"Bearer ".$this->wise_api_key
+            ])->get('https://wise.com/gateway/v2/profiles/'.$this->wise_profile_id.'/acquiring/payment-request-summaries',[
+                'requestType' => 'SINGLE_USE',
+                'status' => 'COMPLETED',
+                'sortBy' => 'UPDATED_AT',
+                'sortOrder' => 'DESC',
+            ])->json();
+            foreach(collect($response['groups']) as $group){
+                if(collect($group['content'])->where('id',$request['payment_id'])->first()!=null){
+                    $paid=1;break;
+                }
+            }
+        }catch(\Exception $e){
+            return [
+                'success' => false,
+                'payment_id'=>"",
+                'message' => $e,
+                'process_data' => $e
+            ];
+        }
+
+        if ($paid == 1) {
+            return [
+                'success' => true,
+                'payment_id'=>$request['payment_id'],
+                'message' => __('nafezly::messages.PAYMENT_DONE'),
+                'process_data' => collect($group['content'])->where('id',$request['payment_id'])->first()
+            ];
+        } else {
+            return [
+                'success' => false,
+                'payment_id'=>$request['payment_id'],
+                'message' => __('nafezly::messages.PAYMENT_FAILED'),
+                'process_data' => $response
+            ];
+        }
+
+    }
+}
