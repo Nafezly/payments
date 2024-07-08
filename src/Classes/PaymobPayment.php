f7b6d8b94ada29023e75cadb37d918e2f4951f86
Saifallak <Saifallak@yahoo.com>
2024-07-08 18:19:43 +0300

    Paymob Flash New Integration

--- src/Classes/PaymobPayment.php
+++ src/Classes/PaymobPayment.php
@@ -2,22 +2,25 @@
 
 namespace Nafezly\Payments\Classes;
 
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Nafezly\Payments\Exceptions\MissingPaymentInfoException;
 use Nafezly\Payments\Interfaces\PaymentInterface;
-use Nafezly\Payments\Classes\BaseController;
 
 class PaymobPayment extends BaseController implements PaymentInterface
 {
-    private $paymob_api_key;
+    private $paymob_public_key;
+    private $paymob_secret_key;
     private $paymob_integration_id;
     private $paymob_iframe_id;
 
 
     public function __construct()
     {
-        $this->paymob_api_key = config('nafezly-payments.PAYMOB_API_KEY');
+        $this->paymob_public_key = config('nafezly-payments.PAYMOB_PUBLIC_API_KEY');
+        $this->paymob_secret_key = config('nafezly-payments.PAYMOB_SECRET_API_KEY');
         $this->paymob_integration_id = config('nafezly-payments.PAYMOB_INTEGRATION_ID');
         $this->paymob_iframe_id = config("nafezly-payments.PAYMOB_IFRAME_ID");
         $this->currency = config("nafezly-payments.PAYMOB_CURRENCY");
@@ -31,34 +34,33 @@ class PaymobPayment extends BaseController implements PaymentInterface
      * @param null $user_email
      * @param null $user_phone
      * @param null $source
-     * @return void
-     * @throws MissingPaymentInfoException
+     * @return array
+     * @throws MissingPaymentInfoException|ConnectionException|RequestException
      */
     public function pay($amount = null, $user_id = null, $user_first_name = null, $user_last_name = null, $user_email = null, $user_phone = null, $source = null)
     {
-        $this->setPassedVariablesToGlobal($amount,$user_id,$user_first_name,$user_last_name,$user_email,$user_phone,$source);
+        $this->setPassedVariablesToGlobal($amount, $user_id, $user_first_name, $user_last_name, $user_email, $user_phone, $source);
         $required_fields = ['amount', 'user_first_name', 'user_last_name', 'user_email', 'user_phone'];
         $this->checkRequiredFields($required_fields, 'PayMob');
 
-        $request_new_token = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/auth/tokens', [
-                "api_key" => $this->paymob_api_key
-            ])->json();
-
-        $get_order = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/ecommerce/orders', [
-                "auth_token" => $request_new_token['token'],
-                "delivery_needed" => "false",
-                "amount_cents" => $this->amount * 100,
-                "items" => []
-            ])->json();
-
-        $get_url_token = Http::withHeaders(['content-type' => 'application/json'])
-            ->post('https://accept.paymobsolutions.com/api/acceptance/payment_keys', [
-                "auth_token" => $request_new_token['token'],
-                "expiration" => 36000,
-                "amount_cents" => $get_order['amount_cents'],
-                "order_id" => $get_order['id'],
+        $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+                'Authorization' => 'Token ' . $this->paymob_secret_key,
+            ])
+            ->post('https://accept.paymob.com/v1/intention/', [
+                "amount" => $this->amount * 100,
+                "currency" => $this->currency,
+                "payment_methods" => [
+                    (int)$this->paymob_integration_id,
+                    // here we can add mobile wallet too if needed according to docs.
+                ],
+                "items" => [],
+                "customer" => [
+                    "first_name" => $this->user_first_name,
+                    "last_name" => $this->user_last_name,
+                    "email" => $this->user_email,
+                ],
                 "billing_data" => [
                     "apartment" => "NA",
                     "email" => $this->user_email,
@@ -74,14 +76,14 @@ class PaymobPayment extends BaseController implements PaymentInterface
                     "last_name" => $this->user_last_name,
                     "state" => "NA"
                 ],
-                "currency" => $this->currency,
-                "integration_id" => $this->paymob_integration_id
-            ])->json();
+            ])
+            ->throw()
+            ->json();
 
         return [
-            'payment_id'=>$get_order['id'],
+            'payment_id' => $response['id'],
             'html' => "",
-            'redirect_url'=>"https://accept.paymobsolutions.com/api/acceptance/iframes/" . $this->paymob_iframe_id . "?payment_token=" . $get_url_token['token']
+            'redirect_url' => "https://accept.paymob.com/unifiedcheckout/?publicKey=$this->paymob_public_key&clientSecret={$response['client_secret']}"
         ];
     }
 
